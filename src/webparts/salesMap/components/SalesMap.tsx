a2c52bdaa6f0d61484da80048002f4a854282313
kyleschaeffer <kyle@spirito.us>
2019-05-31 20:09:13 -0400

    Add datamaps

--- src/webparts/salesMap/components/SalesMap.tsx
+++ src/webparts/salesMap/components/SalesMap.tsx
@@ -1,5 +1,7 @@
 import { WebPartContext } from '@microsoft/sp-webpart-base';
 import * as React from 'react';
+import Datamap from 'react-datamaps';
+
 import styles from './SalesMap.module.scss';
 
 export interface ISalesMapProps {
@@ -10,8 +12,24 @@ export default class SalesMap extends React.Component<ISalesMapProps, {}> {
   public render(): React.ReactElement<ISalesMapProps> {
     return (
       <div className={styles.map}>
-        <h1>Map</h1>
+        <h1 className={styles.title}>Sales by State</h1>
+
+        <Datamap
+          scope="usa"
+          fills={{
+            defaultFill: '#eee',
+            gt50: '#555',
+          }}
+          data={this.mapData()}
+        />
       </div>
     );
+  }
+
+  private mapData(): any {
+    return {
+      VA: { fillKey: 'gt50' },
+      CA: { fillKey: 'gt50' },
+    };
   }
 }
